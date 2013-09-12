5e12bad6f57c5c730762657c03563d59179ab510
Sean Blezard <sean@optimiseagility.com>
2013-09-12 23:50:55 +0100

    Another small code smell

--- app/lib/domain_model.rb
+++ app/lib/domain_model.rb
@@ -13,14 +13,13 @@ module DomainModel
 		@violations.size == 0		
 	end
 
-	def initialize(h={})
-  	@@attributes_to_ignore=[:class, :type, :extra_errors, :queued_events, :errors, "type",  "class", "extra_errors", "queued_events", "errors"] # These won't get picked up in the automatic hash-based setters e.g. initialize(h) and attributes=(h) where h is the has of attributes and values
-  	self.attributes=h
+	def initialize(attributes={})
+  	self.attributes=attributes
   	@violations = []
 	end
 
-	def attributes=(h) 
- 		h.each {|k,v| send("#{k}=",v) unless !self.respond_to?(k) || @@attributes_to_ignore.include?(k)}
+	def attributes=(attributes) 
+ 		attributes.each {|attribute_key,attribute_value| send("#{attribute_key}=",attribute_value) unless !self.respond_to?(attribute_key)}
  	end
 
  	#
